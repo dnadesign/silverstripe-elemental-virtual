2d414a3d588c0ddf309f0518040daa1b9e7c6ff3
Nivanka Fonseka <nivanka@silverstripers.com>
2018-09-19 07:50:08 +0530

    removed the undefined variables, removed the whole lines as the variables were never used below

--- src/Extension/BaseElementExtension.php
+++ src/Extension/BaseElementExtension.php
@@ -168,10 +168,8 @@ class BaseElementExtension extends DataExtension
                 $wasPublished = false;
             }
             if ($firstVirtual) {
-                $origParentID = $this->ParentID;
-                $origSort = $this->Sort;
 
-                $clone = $this->duplicate(false);
+                $clone = $this->owner->duplicate(false);
 
                 // set clones values to first virtual's values
                 $clone->ParentID = $firstVirtual->ParentID;
